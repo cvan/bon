df270e1a49bbeb180b83b44f558bdf0a5e5d69e2
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2016-12-15 03:58:53 -0800

    clean up `fetch` file and upgrade `isomorphic-fetch`

--- src/app/util/fetch.js
+++ src/app/util/fetch.js
@@ -1,35 +1,34 @@
-// Fetch Util
+// Fetch Util.
 import fetch_ from 'isomorphic-fetch';
 
 class FetchUtil {
-    constructor() {
-        this.fetch = fetch_.bind(undefined); // this solves an invocation error problem in chrome, according to https://github.com/matthew-andrews/isomorphic-fetch/pull/20
-    }
+  constructor () {
+  }
 
-    postJSON(target, data) {
-        return this.fetch(target, {
-            method: 'POST',
-            credentials: 'include',
-            body: JSON.stringify(data),
-            headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-            }
-        });
-    }
+  postJSON (target, data) {
+    return this.fetch(target, {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+  }
 
-    getJSON(target) {
-        return this.fetch(target, {
-            method: 'GET',
-            credentials: 'include',
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-    }
-
+  getJSON (target) {
+    return this.fetch(target, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+  }
 };
 
-// Singleton
+// Singleton.
 let instance = new FetchUtil();
+
 export default instance;
